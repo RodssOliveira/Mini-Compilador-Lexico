7d7927d8f17f464c6772b376b103b374973dd54a
RodssOliveira <rodrigocanal07@gmail.com>
2020-04-03 12:18:35 -0300

    Upgrade no código

--- compilador.py
+++ compilador.py
@@ -3,66 +3,119 @@ import re
 #Txt
 #code = open("C:\\Users\\Rodrigo\\Desktop\\code.c").read()
 
-code = 'portuguesa = corinthians + 2*palmeiras'
-print('Codigo Bruto')
-print(code)
-
-raw_code = re.split('(\(|\)|\+| |\=|\*|\/|\\n|\\t)', code)
-clean_raw_code = [i for i in raw_code if i.strip()]
-
-print('Codigo limpo')
-print(clean_raw_code)
-
-print('------------')
-print('| Resultado |')
-print('------------')
-
-count = 1
-lexic_list_name = []
-lexic_list_token = []
-
-lexic_list_name_str = ''
-lexic_list_token_str = ''
-
-id_list = ['=', '/', '*', '+', '-', '(', ')', '{', '}']
-
-#Mostrando o nome da senteça
-for sentence in clean_raw_code:
-    if sentence in id_list:
-        token = '<' +sentence+ '>'
-        lexic_list_name.append(token) if sentence else None
-        lexic_list_name_str += token
-    else:
-        token = '<' +sentence + ',' + str(count) + '>'
-        lexic_list_name.append(token) if sentence else None
-        lexic_list_name_str += token
-        count += 1
-
-#Mostrando o token da senteça
-count = 1
-for sentence in clean_raw_code:
-    sentence_type = 'num' if sentence.isdigit() else 'id'
-    if sentence in id_list:
-        token = '<' +sentence+ '>'
-        lexic_list_token.append(token) if sentence else None
-        lexic_list_token_str += token
-    else:
-        token = '<' +sentence_type + ',' + str(count) + '>'
-        lexic_list_token.append(token) if sentence else None
-        lexic_list_token_str += token
-        count += 1
-
-print('Com nome da variavel')
-print('Lista: ', lexic_list_name)
-print('String: ', lexic_list_name_str)
-print('\nCom nome do token')
-print('Lista: ', lexic_list_token)
-print('String: ', lexic_list_token_str)
-
+code = '''
+float limitedSquare(x) float x {
+/* retorna x ao quadrado, mas nunca mais do que 100 */
+return (x<=-10.0 || x>=10.0)?100:x*x;
+}
 
 '''
-portuguesa = corinthians + 2*palmeiras
-Comprimento = 2*3.1416*raio
-R = (a+5)*(b-1)
-x = (2*y-3)/z
-'''
+
+def code_cleaner(code):
+    
+    #Removing comments
+    raw_code = ''.join(re.split('\/\*(.|\n)*?\*\/', code)) # /* */
+    raw_code = ''.join(re.split('[^:]\/\/.*', raw_code)) # //
+
+    #Implementar separado
+    #Spliting Logical operators <= >= || && == != (\<\=|\>\=|\|\||\&\&|\=\=|\!\=)
+    #raw_code = re.split('(\<\=|\>\=|\|\||\&\&|\=\=|\!\=)', raw_code)
+    
+    #Spliting Arithmetic operators += -= ++ -- = - / * % (\+\=|\-\=|\+\+|\-\-|\+|\-|\/|\*|\%)
+    #raw_code = [re.split('(\+\=|\-\=|\+\+|\-\-|\+|\-|\/|\*|\%)', sentence) for sentence in raw_code]
+    
+    #Spliting symbols (\(|\)|\{|\}|\;|\\n|\\t)
+    #raw_code = [re.split('(\(|\)|\{|\}|\;|\\n|\\t)', sentence) for sentence in raw_code]
+    
+    raw_code = re.split('(\<\=|\>\=|\|\||\&\&|\=\=|\!\=|\:|\?|\+\=|\-\=|\+\+|\-\-|\+|\-|\/|\*|\%|\(|\)|\{|\}|\;|\\n|\\t| |)', raw_code)
+    clean_raw_code = [i for i in raw_code if i.strip()]
+
+    return clean_raw_code
+
+def sentence_type(sentence):
+    table = {
+        'Comparison': ['<=', '>=', '==', '!=', '>', '<'],
+        'Operator': ['+', '-', '*', '/', '%', '^'],
+        'Logical': ['&&', '||', '!'],
+        '=': '=',
+        'Float': ['float'],
+        'Return': ['return'],
+        '(':'(',
+        ')':')',
+        '{':'{',
+        '}':'}',
+        ';':';',
+        '?': '?',
+        ':': ':'
+    }
+
+    #Check if the sentence is in the table
+    for key in table:
+        for i in table[key]:
+            if sentence == i:
+                return key + ' ' + str(sentence)
+
+    #Check if is float or int
+    try:
+        if sentence.isdigit():
+            Number = int(sentence)
+            return 'Num ' + str(sentence)
+        else:
+            Number = float(sentence)
+            return 'Num ' + str(sentence) #Token Float?
+    except ValueError:
+        return 'id ' + str(sentence)
+
+
+def compiler(code):
+    print('-------------')
+    print('|Codigo Bruto|')
+    print('-------------')
+    print(code)
+
+    code = code_cleaner(code)
+
+    print('-------------')
+    print('|Codigo limpo|')
+    print('-------------')
+    print(code)
+
+    print('\n------------')
+    print('| Resultado |')
+    print('------------')
+
+    count = 1
+
+    lexic_final= ''
+    token_final = ''
+
+    #Lexema
+    for sentence in code:
+        token = '<' +sentence+ '>'
+        lexic_final += token
+
+    #Token
+    count = 1
+    id_list = []
+    id_sentences = ['id', 'Num', 'Logical', 'Comparison', 'Operator']
+    for sentence in code:
+        token = sentence_type(sentence)
+        token = token.split(' ')
+        if token[0] in id_sentences:
+            if token[1] in id_list:
+                id_index = id_list.index(token[1]) + 1
+                token_final += '<'+str(token[0])+','+str(id_index)+'>'
+            else:
+                id_list.append(token[1])
+                id_index = id_list.index(token[1]) + 1
+                token_final += '<'+str(token[0])+','+str(id_index)+'>'
+        else:
+            token_final += '<'+ str(token[0]) + '>'
+        
+    print('Fluxo de Lexemas')
+    print('String: ', lexic_final)
+    print('\nFluxo de Tokens')
+    print('String: ', token_final)
+
+
+compiler(code)
